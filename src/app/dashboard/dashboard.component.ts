0bdad67d0a886e53b194590776f85a1b977128fb
chahatsharma01 <chahatsharma.01march2001@gmail.com>
2023-09-12 09:38:37 +0530

    secondd commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,11 +22,13 @@ export class DashboardComponent implements OnInit {
   input = [
     { id: 611, countryCode: "AU", venue: "Pinjarra", date: "1692591360" },
     { id: 612, countryCode: "AU", venue: "Pinjarra", date: "1692604800" },
-    { id: 613, countryCode: "GB", venue: "Redcliffe", date: "1692623100" },
+    { id: 613, countryCode: "GB", venue: "Redcliffe", date: "1692625100" },
     { id: 614, countryCode: "GB", venue: "Brighton", date: "1692623100" },
     { id: 615, countryCode: "US", venue: "Lingfield", date: "1692634200" },
     { id: 616, countryCode: "RA", venue: "Philadelphia", date: "1692636000" },
     { id: 617, countryCode: "GB", venue: "Landgrob", date: "1692626000" },
+    { id: 618, countryCode: "GB", venue: "Redcliffe", date: "1692625100" },
+
   ];
 
   filterData(countryCode: string) {
